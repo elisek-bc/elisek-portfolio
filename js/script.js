7fe929d4cd07d334c9508740812ac684e33d86aa
elisek-bc <elisekppns@gmail.com>
2019-08-07 09:47:29 +0200

    make the homepage work smoother but with glitches and no working links

--- js/script.js
+++ js/script.js
@@ -5,16 +5,25 @@ document.addEventListener('DOMContentLoaded', function() {
     let handle = wrapper.querySelector(".handle");
 
     wrapper.addEventListener("mousemove", function(e){
-    let calcOppWidth = window.innerWidth-(e.clientX - (window.innerWidth/2 - e.clientX)) + 'px'
-    handle.style.left = calcOppWidth;
-    topLayer.style.width = calcOppWidth;
+        console.log(e.clientY);
+    if (e.clientY > 80){
+        let calcOppWidth = window.innerWidth-(e.clientX - (window.innerWidth/2 - e.clientX)) + 'px'
+        handle.style.left = calcOppWidth;
+        topLayer.style.width = calcOppWidth;
 
-    if (e.clientX + 'px' < calcOppWidth) {
-        bottomLayer.style.filter = "grayscale(1)";
-        topLayer.style.filter = "none";
-    } else if (e.clientX + 'px' > calcOppWidth){
-        topLayer.style.filter = "grayscale(1)";
+        if (e.clientX + 'px' < calcOppWidth) {
+            bottomLayer.style.filter = "grayscale(1)";
+            topLayer.style.filter = "none";
+        } else if (e.clientX + 'px' > calcOppWidth){
+            topLayer.style.filter = "grayscale(1)";
+            bottomLayer.style.filter = "none";
+        }
+    } else {
+        handle.style.left = 50+'%';
+        topLayer.style.width = 50+'%';
         bottomLayer.style.filter = "none";
-    } 
+        topLayer.style.filter = "none";
+    };
     });
+    
 });
