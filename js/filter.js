54b1ddc726393419a315dd5119c556c2f76ae05b
elisek-bc <elisekppns@gmail.com>
2019-08-14 17:07:02 +0200

    add biggest part of portfolio page

--- js/filter.js
+++ js/filter.js
@@ -3,8 +3,8 @@
 
 var buttons = document.getElementsByClassName('buttons')[0];
 var buttonArray = Array.from(buttons.querySelectorAll('button'));
-var allItems = document.querySelectorAll('.item');
-console.log(buttonArray);
+var allItems = Array.from(document.querySelectorAll('.item'));
+console.log(allItems);
 
 const toggleActiveClass = (active) => {
     buttonArray.forEach(button => {
@@ -16,18 +16,32 @@ const toggleActiveClass = (active) => {
 function toggleItems(dataClass){
     if(dataClass === 'all'){
         for(let i = 0; i<allItems.length; i++){
-            allItems[i].style.display = 'block';
+            allItems[i].style.display = 'flex';
         }
     }else{
-        for(let i = 0; i<allItems.length; i++)
-            allItems[i].dataset.class === dataClass ? allItems[i].style.display = 'block' : allItems[i].style.display = 'none';
+        for(let i = 0; i<allItems.length; i++){
+            allItems[i].dataset.filter === dataClass ? allItems[i].style.display = 'flex' : allItems[i].style.display = 'none';
+        }
     }
 }
  
 buttonArray.forEach((button) => {
     button.addEventListener('click', function(){
         toggleActiveClass(button);
-        toggleItems(button.dataset.class);
+        toggleItems(button.dataset.filter);
     })
 });
 
+// SIZING 
+
+const setImageHeight = () => {
+    allItems.forEach((item) => {
+    const image = item.querySelector('.portfolio-image');
+    const text = item.querySelector('.portfolio-text');
+    console.log(text.clientHeight)
+    image.style.height = text.clientHeight + 200 + "px";
+    })
+}
+
+setImageHeight()
+window.addEventListener('resize', setImageHeight);
