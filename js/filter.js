73b86271df87c47c84765ff1e6854f4d88d914bd
elisek-bc <elisekppns@gmail.com>
2019-08-14 11:10:34 +0200

    stylized filter on development and design page

--- js/filter.js
+++ js/filter.js
@@ -1,21 +1,33 @@
 
+// TOGGLE BUTTON CLASS
+
 var buttons = document.getElementsByClassName('buttons')[0];
 var buttonArray = Array.from(buttons.querySelectorAll('button'));
+var allItems = document.querySelectorAll('.item');
 console.log(buttonArray);
 
+const toggleActiveClass = (active) => {
+    buttonArray.forEach(button => {
+      button.classList.remove('active');
+    })
+    active.classList.add('active');
+}
+ 
+function toggleItems(dataClass){
+    if(dataClass === 'all'){
+        for(let i = 0; i<allItems.length; i++){
+            allItems[i].style.display = 'block';
+        }
+    }else{
+        for(let i = 0; i<allItems.length; i++)
+            allItems[i].dataset.class === dataClass ? allItems[i].style.display = 'block' : allItems[i].style.display = 'none';
+    }
+}
+ 
 buttonArray.forEach((button) => {
-    const handleClick = () => { 
-        var buttonSpan = button.querySelector('span');  
-        buttonSpan.classList.toggle('active');
-        
-    };
-
-    button.addEventListener('click', handleClick);
+    button.addEventListener('click', function(){
+        toggleActiveClass(button);
+        toggleItems(button.dataset.class);
+    })
 });
-
-    
-
-
-
-
 
